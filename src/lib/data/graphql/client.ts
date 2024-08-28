6ad47bf564f57d744e2e2c2d82178c87f62c7170
Thinker-Dev <107056866+Thinker-Dev@users.noreply.github.com>
2024-08-28 20:31:10 +0200

    commit

--- src/lib/data/graphql/client.ts
+++ src/lib/data/graphql/client.ts
@@ -5,7 +5,7 @@ export const { getClient } = registerApolloClient(() => {
   return new ApolloClient({
     cache: new InMemoryCache(),
     link: new HttpLink({
-      uri: "http://localhost:4000",
+      uri: "https://muse-backend-rqm8.onrender.com",
     }),
   });
 });
